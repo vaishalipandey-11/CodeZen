6828f089cff3416ee125076095032ee521cdd7c6
vaishali pandey <pandeyvaishali002@gmail.com>
2025-02-09 20:36:56 +0530

    Setup frontend structure and initialize project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
 import { Route,Routes } from "react-router-dom";
+import Home  from "./pages/Home";
 
 function App() {
   return (
-    <div>
+    <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter ">
       <Routes>
         <Route path="/" element={<Home/>}></Route>
       </Routes>
---
+++ src/components/HomePage/HighlightText.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function HighlightText({text}) {
+  return (
+     <span className='font-bold text-richblack-200 '>
+        {" "}
+        {text}
+        </span>
+  )
+}
