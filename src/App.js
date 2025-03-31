16b4708f1dc0b6b45a804907898ef822d1398835
vaishali pandey <pandeyvaishali002@gmail.com>
2025-03-31 23:46:45 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import { Route,Routes } from "react-router-dom";
 import Home  from "./pages/Home";
+// import toast from "react-hot-toast";
+import Navbar from "./components/common/Navbar"
+// import PrivateRoute from "./components/core/Auth/PrivateRoute";
+
 
 function App() {
   return (
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter ">
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
       </Routes>
